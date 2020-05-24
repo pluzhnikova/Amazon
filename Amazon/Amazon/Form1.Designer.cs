420849d4d4fa8c211429b55eaee2dd9c2d7d2898
pluzhnikova <58787450+pluzhnikova@users.noreply.github.com>
2020-05-24 16:44:57 +0300

    amazon

---
+++ Amazon/Amazon/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Amazon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Col1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Col4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.MistyRose;
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.trackBar1);
+            this.panel1.Controls.Add(this.radioButton7);
+            this.panel1.Controls.Add(this.radioButton8);
+            this.panel1.Controls.Add(this.radioButton9);
+            this.panel1.Controls.Add(this.radioButton4);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Controls.Add(this.radioButton6);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(1, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(219, 669);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(161, 558);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 33);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "5";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(3, 468);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(163, 33);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Число книг:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(-8, 504);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 5;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(234, 69);
+            this.trackBar1.TabIndex = 9;
+            this.trackBar1.TabStop = false;
+            this.trackBar1.Value = 5;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton7.Location = new System.Drawing.Point(3, 401);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(164, 37);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "JavaScript";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton8.Location = new System.Drawing.Point(3, 358);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(117, 37);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "Pascal";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            this.radioButton8.CheckedChanged += new System.EventHandler(this.radioButton8_CheckedChanged);
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton9.Location = new System.Drawing.Point(3, 315);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(100, 37);
+            this.radioButton9.TabIndex = 6;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "Swift";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            this.radioButton9.CheckedChanged += new System.EventHandler(this.radioButton9_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton4.Location = new System.Drawing.Point(3, 274);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(109, 37);
+            this.radioButton4.TabIndex = 5;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Kotlin";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            this.radioButton4.CheckedChanged += new System.EventHandler(this.radioButton4_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton5.Location = new System.Drawing.Point(3, 231);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(85, 37);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "C++";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton6.Location = new System.Drawing.Point(3, 188);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(94, 37);
+            this.radioButton6.TabIndex = 3;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "PHP";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(3, 144);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(93, 37);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Java";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(3, 101);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(74, 37);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "C#";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Montserrat", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(3, 58);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(127, 37);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Python";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.listView1);
+            this.panel2.Location = new System.Drawing.Point(218, 1);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1110, 669);
+            this.panel2.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Col1,
+            this.Col2,
+            this.Col3,
+            this.Col4});
+            this.listView1.Font = new System.Drawing.Font("Montserrat", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.ShowGroups = false;
+            this.listView1.Size = new System.Drawing.Size(1122, 672);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listView1_MouseDoubleClick);
+            // 
+            // Col1
+            // 
+            this.Col1.Text = "Название";
+            this.Col1.Width = 390;
+            // 
+            // Col2
+            // 
+            this.Col2.Text = "Автор";
+            this.Col2.Width = 141;
+            // 
+            // Col3
+            // 
+            this.Col3.Text = "Цена, руб";
+            this.Col3.Width = 80;
+            // 
+            // Col4
+            // 
+            this.Col4.Text = "Издательство";
+            this.Col4.Width = 100;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1324, 667);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Col1;
+        private System.Windows.Forms.ColumnHeader Col2;
+        private System.Windows.Forms.ColumnHeader Col3;
+        private System.Windows.Forms.ColumnHeader Col4;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Amazon/Amazon/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Amazon
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            trackBar1.Value = 5;
+        }
+        public int i = 2;
+        public void FindBooks(string html, int amount) {
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AutomaticDecompression = DecompressionMethods.GZip,
+            };
+
+            using (var client = new HttpClient(handler))
+            {
+                using (HttpResponseMessage response = client.GetAsync(html).Result)
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        string htmlCode = content.ReadAsStringAsync().Result;
+
+                        MatchCollection author = Regex.Matches(htmlCode, "<div class=\"product-author\">(\n|\r|\r\n).*?<a href=\".*?\" title=\"(.*?)\"");
+                        MatchCollection name = Regex.Matches(htmlCode, "data-name=\"(.*?)\"");
+                        MatchCollection price = Regex.Matches(htmlCode, "data-discount-price=\"(.*?)\"");
+                        MatchCollection pubhouse = Regex.Matches(htmlCode, "data-pubhouse=\"(.*?)\"");
+                        MatchCollection id = Regex.Matches(htmlCode, "data-product-id=\"(.*?)\"");
+                        MatchCollection dir = Regex.Matches(htmlCode, "data-dir=\"(.*?)\"");
+                        Match crawler = Regex.Match(htmlCode, ".*?\"pagination-next__text\" href=\"(.*?)\"");
+                       
+                        
+                            for (int i = 0; i < Math.Min(amount, author.Count); i++)
+                            {
+                                ListViewItem item = new ListViewItem { Text = name[i].Groups[1].Value.ToString() };
+                                item.SubItems.Add(author[i].Groups[2].Value);
+                                item.SubItems.Add(price[i].Groups[1].Value.ToString());
+                                item.SubItems.Add(pubhouse[i].Groups[1].Value.ToString());
+                                string s = "https://www.labirint.ru/ebooks/10000623/";
+                                s = s.Replace("ebooks", dir[i].Groups[1].Value.ToString());
+                                s = s.Replace("10000623", id[i].Groups[1].Value.ToString());
+                                item.SubItems.Add(s);
+                                listView1.Items.Add(item);
+                            }
+                    }
+                }
+            }
+            }
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/python/?stype=0", trackBar1.Value);
+
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    while (listView1.Items.Count != trackBar1.Value)
+                    {
+                        string scrawler = "https://www.labirint.ru/search/python/?stype=0&page=" + i.ToString();
+                        FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                    }
+                }
+            }
+        }
+        private void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+           System.Diagnostics.Process.Start(listView1.SelectedItems[0].SubItems[4].Text);
+        }
+        
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton2.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/c%23/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    while (listView1.Items.Count != trackBar1.Value)
+                    {
+                        string scrawler = "https://www.labirint.ru/search/c%23/?stype=0&page=" + i.ToString();
+                        FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                    }
+                }
+            }
+        }
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/Java/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    while (listView1.Items.Count != trackBar1.Value)
+                    {
+                        string scrawler = "https://www.labirint.ru/search/Java/?stype=0&page=" + i.ToString();
+                        FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                    }
+                }
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            label3.Text = trackBar1.Value.ToString();
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton6.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/php/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    while (listView1.Items.Count != trackBar1.Value)
+                    {
+                        string scrawler = "https://www.labirint.ru/search/php/?stype=0&page=" + i.ToString();
+                        FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                    }
+                }
+            }
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton5.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/c%2B%2B/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    while (listView1.Items.Count != trackBar1.Value)
+                    {
+                        string scrawler = "https://www.labirint.ru/search/c%2B%2B/?stype=0&page=" + i.ToString();
+                        FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                    }
+                }
+            }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton4.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/kotlin%20программирование/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    try
+                    {
+                        while (listView1.Items.Count != trackBar1.Value)
+                        {
+                            string scrawler = "https://www.labirint.ru/search/kotlin%20программирование/?stype=0&page=" + i.ToString();
+                            FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+            }
+
+        private void radioButton9_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton9.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/swift%20программирование/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    try
+                    {
+                        while (listView1.Items.Count != trackBar1.Value)
+                        {
+                            string scrawler = "https://www.labirint.ru/search/swift%20программирование/?stype=0&page=" + i.ToString();
+                            FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+        }
+        private void radioButton8_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton8.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/pascal%20программирование/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    try
+                    {
+                        while (listView1.Items.Count != trackBar1.Value)
+                        {
+                            string scrawler = "https://www.labirint.ru/search/pascal%20программирование/?stype=0&page=" + i.ToString();
+                            FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButton7.Checked)
+            {
+                listView1.Items.Clear();
+                FindBooks("https://www.labirint.ru/search/javascript/?stype=0", trackBar1.Value);
+                if (listView1.Items.Count == 60 && listView1.Items.Count < trackBar1.Value)
+                {
+                    try
+                    {
+                        while (listView1.Items.Count != trackBar1.Value)
+                        {
+                            string scrawler = "https://www.labirint.ru/search/javascript/?stype=0&page=" + i.ToString();
+                            FindBooks(scrawler, trackBar1.Value - listView1.Items.Count); i++;
+                        }
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+}
